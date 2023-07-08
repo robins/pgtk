c665b8f8d0b4457192d23b606dcdeba5f2113fad
Robins Tharakan <tharakan@gmail.com>
2023-07-09 06:57:18 +0930

    Add minor documentation + Sample output / Remove sleep to waldump.sh

--- wal/waldump.sh
+++ wal/waldump.sh
@@ -1,8 +1,18 @@
+# Uses pg_waldump to process all WALs into Text files (1:1 mapping)
+#
+# Sample Output
+# -------------
+# $ ./dumpwals.sh
+# 000000010000000000000005.. Skipped
+# 000000010000000000000006.. reprocesssing empty file.. Done
+# 000000010000000000000007.. reprocesssing empty file.. Done
+# 000000010000000000000008.. reprocesssing empty file.. Done
+# 000000010000000000000009.. Done
+
+
 BINDIR='/home/ubuntu/proj/tempdel/bin'
 WALDIR='/home/ubuntu/proj/tempdel/data/pg_wal'
 WALTXTDIR='/home/ubuntu/proj/tempdel/waltxt'
-
-echo "Intentionally hiding any pg_waldump errors"
 
 for wal1 in $(ls ${WALDIR}/0000* | grep -v done |  xargs -i basename {} )
 do
@@ -15,7 +25,8 @@ do
     echo -n ".. reprocesssing empty file"
   fi
 
+  # Intentionally hiding any pg_waldump errors
   ${BINDIR}/pg_waldump ${WALDIR}/${wal1} 2>/dev/null | sed -e "s/^/${wal1} /g" > ${WALTXTDIR}/waldump_${wal1}.txt
   echo ".. Done"
-  sleep 0.1
+  
 done
